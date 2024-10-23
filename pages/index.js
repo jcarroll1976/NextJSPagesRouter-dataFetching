b3e42179f46ed2ecb5c53fadf11a1eacdea64019
Joshua Carroll <carrolljosh1976@gmail.com>
2024-10-22 21:35:09 -0400

    created dynamic [pid}.js page for dynamic pages for different products

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,16 @@
 import path from 'path';
 import fs from 'fs/promises';
 import { notFound } from 'next/navigation';
+import Link from 'next/link';
 
 export default function HomePage(props) {
   const {products} = props;
 
   return (
     <ul>
-      {products.map((product) => (<li key={product.id}>{product.title}</li>))}
+      {products.map((product) => (<li key={product.id}>
+                                    <Link href={`/${product.id}`}>{product.title}</Link>
+                                  </li>))}
     </ul>
   );
 }
@@ -18,11 +21,23 @@ export async function getStaticProps() {
   const filePath = path.join(process.cwd(), 'data', 'dummy-backend.json');
   const jsonData = await fs.readFile(filePath);
   const data = JSON.parse(jsonData);
+
+  if (!data) {
+    return {
+      redirect: {
+        destination: "/no-data",
+      }
+    }
+  }
+  if (data.products.length === 0) {
+    return {notFound: true};
+  }
+
   return {
       props: {
       products: data.products,
              },
-      revalidate: 10,
-      notFound: true
+      revalidate: 10
+      
         }
 }
